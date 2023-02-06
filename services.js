170cfc06d92824b7ab9a1c1ef66b50c8ff97f5b1
Nicholas Adams <njadams.722@gmail.com>
2023-02-06 15:58:08 -0600

    commit message

---
+++ services.js
@@ -0,0 +1,5 @@
+//TODO Import services data from database.js
+//TODO Store return in variable
+//TODO Create and export function that returns servicesHTML list 
+//TODO Iterate through services array
+//TODO Return servicesHTML
